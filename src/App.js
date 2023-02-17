56ff8a1338fe21bbef089a673d535d726a5a6bdb
German Paolini Laumann <paolinilaumann@gmail.com>
2023-02-17 20:01:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+       
+      <header className="App-header">
+       
+      <NavBar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
